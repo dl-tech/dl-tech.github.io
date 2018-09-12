c6e4b956dd20aba4c9809e6dfcca0d4ebad6c995
dl-tech <elxbass@gmail.com>
2018-09-12 18:50:51 -0300

    preparing to add cards

--- scripts/preview.js
+++ scripts/preview.js
@@ -1,3 +1,4 @@
+var enableImageResize = true;
 
 function previewNextImage(previewIndex, startAt) {
 
@@ -7,39 +8,44 @@ function previewNextImage(previewIndex, startAt) {
 
 		// more info about select files to upload limit on windows chrome at:
 		// https://stackoverflow.com/questions/15851751/what-is-the-max-number-of-files-to-select-in-an-html5-multiple-file-input
-	
-		var previewNode = document.createElement("div");
-		previewNode.setAttribute("class", "sample");
-		previewNode.appendChild(document.createElement("img"));
-		previewNode.appendChild(document.createElement("div"));
-		previewNode.lastChild.appendChild(document.createElement("span"));
-		previewNode.lastChild.appendChild(document.createElement("span"));
-		previewNode.lastChild.appendChild(document.createElement("span"));
-
-		var gallery = document.getElementById("gallery");
 
 		var b = new FileReader();
 
+        //TODO don't declare functions in a bucle
+
 		b.onload = function(e) {
 
-			var n = previewNode.cloneNode(true);
-            n.firstChild.onload = function () {
+		    var gallery = document.getElementById("gallery");
+		    var previewNode = document.createElement("div");
 
-				if ( this.width > 50 || this.height > 50 ) {
+		    previewNode.setAttribute("class", "sample");
+
+		    var image = previewNode.appendChild(document.createElement("img"));
+
+		    previewNode.appendChild(document.createElement("div"));
+
+		    var filename = previewNode.lastChild.appendChild(document.createElement("span"));
+		    var percent = previewNode.lastChild.appendChild(document.createElement("span"));
+		    var labelname = previewNode.lastChild.appendChild(document.createElement("span"));
+
+            image.onload = function () {
+
+				if ( enableImageResize && (this.width > 50 || this.height > 50) ) {
 
 					uploadContainer.file("files/"+previewIndex+".jpg", resize(this), {base64: true});
 				}
 				else {
 
-					uploadContainer.file("files/"+previewIndex+".jpg", lastpull[previewIndex], {base64: true});
+					uploadContainer.file("files/"+previewIndex+".jpg", lastpull[previewIndex]);
 				}
 
 				previewNextImage( previewIndex + 1, startAt );
             }
-			n.firstChild.setAttribute("src", e.target.result);
-			n.lastChild.firstChild.innerHTML = lastpull[previewIndex].name;
-			n.lastChild.firstChild.nextSibling.setAttribute("id", "status-" + (alreadyRequested+previewIndex));
-			n.lastChild.lastChild.setAttribute("id", "clase-" + (alreadyRequested+previewIndex));
+			image.setAttribute("src", e.target.result);
+
+			filename.innerHTML = lastpull[previewIndex].name;
+			percent.setAttribute("id", "status-" + (alreadyRequested+previewIndex));
+			labelname.setAttribute("id", "clase-" + (alreadyRequested+previewIndex));
 
 			gallery.appendChild(n);
 		}
